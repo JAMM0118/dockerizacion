f89ebca62adedd78546961ec3cf080be91c6889a
JAMM0118 <jhon.alejandro.martinez@correounivalle.edu.co>
2024-12-11 08:28:19 -0500

    first commit

---
+++ src/main/java/org/facengineer/Model/RtcTrelloModel.java
@@ -0,0 +1,23 @@
+package org.facengineer.Model;
+
+public class RtcTrelloModel {
+    String trelloname;
+    String rtcname;
+
+    public String getTrelloname() {
+        //esto es un 
+        return trelloname;
+    }
+
+    public void setTrelloname(String trelloname) {
+        this.trelloname = trelloname;
+    }
+
+    public String getRtcname() {
+        return rtcname;
+    }
+
+    public void setRtcname(String rtcname) {
+        this.rtcname = rtcname;
+    }
+}
